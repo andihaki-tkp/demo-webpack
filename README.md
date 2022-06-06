474c3bef3a82c689574ff6ddd73547b52dea7324
andihaki-tkp <55688617+andihaki-tkp@users.noreply.github.com>
2022-06-06 09:30:05 +0700

    fix: requirements

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 This is a bare minimum project with react and unify. You have to fork this repo and create webpack config to meets the basic requirements below. There are modules and plugins installed by default on packages.json, but feel free to adjust based on your needs.
 
 The basic requirements:
-- [ ] Support css
+- [x] Support css
 - [ ] Create multiple bundle output with this criteria:
-  - [ ] Dependency react, react-dom, react-emotion, and emotion use chunk name `framework.[contenthash].js`
-  - [ ] All unify dependencies use chunk name `unify.[contenthash].js`
-  - [ ] The rest node_modules dependencies use chunk name `vendor.[contenthash].js`
-  - [ ] The runtime code use chunk name `runtime.[contenthash].js`
-- [ ] All assets should be minified in production build, except in development env
-- [ ] All chunk name should has `contenthash` in production build, except in development env
+  - [x] Dependency react, react-dom, react-emotion, and emotion use chunk name `framework.[contenthash].js`
+  - [x] All unify dependencies use chunk name `unify.[contenthash].js`
+  - [x] The rest node_modules dependencies use chunk name `vendor.[contenthash].js`
+  - [x] The runtime code use chunk name `runtime.[contenthash].js`
+- [x] All assets should be minified in production build, except in development env
+- [x] All chunk name should has `contenthash` in production build, except in development env
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const mode = process.env.NODE_ENV;
+const isProd = mode === 'production';
+
+module.exports = {
+  mode: mode,
+  entry: {
+    main: './src/index',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProd ? '[name].[contenthash].js' : '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,  "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: { 
+        framework: { 
+          chunks: 'all', 
+          test: /[\\/]node_modules[\\/](react|react-dom|react-emotion|emotion)[\\/]/, 
+          name: 'framework',
+        },
+        unify: { 
+          chunks: 'all', 
+          test: /[\\/]node_modules[\\/](unify-).*[\\/]/, 
+          name: 'unify',
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: "all",
+          priority: -10,
+        },
+      },
+    },
+    ...(isProd && {
+      minimize: true,
+      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()]
+    })
+  },
+  performance: {
+    hints: false, // @todo: how to fix warning asset size limit
+  },
+}
